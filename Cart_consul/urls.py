6f4d64c90d4a58ddb9a485523f867a6cdb38b0b4
jeanclaudesperandezi <sounguejc@gmail.com>
2025-05-13 21:09:36 +0100

    template v01

--- Cart_consul/urls.py
+++ Cart_consul/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ambassade import views
 
 urlpatterns = [
+    path('', views.accueil),
     path('admin/', admin.site.urls),
 ]
--- ambassade/views.py
+++ ambassade/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def accueil(request):
+    return render(request, template_name="accueil.html")
+
 # Create your views here.
